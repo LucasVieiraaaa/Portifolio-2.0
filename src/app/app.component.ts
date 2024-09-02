a73d60f2e7dcc1872e2a837e7a30a241e3fc6dee
unknown <lucasvieirasouzaa@gmail.com>
2024-09-02 16:42:17 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 })
 
 export class AppComponent {
-  title = 'portifolio-lucas';
+  title = 'portfolio-lucas';
 
   WhatsApp() {
     var x = "5531975580097";
