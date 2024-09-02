c687e985284ab4fb4975add5ae781c6a16258797
unknown <lucasvieirasouzaa@gmail.com>
2024-09-02 00:45:18 -0300

    Responsivity Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatTooltipModule],
+  imports: [RouterOutlet,MatTooltipModule,MatProgressBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
