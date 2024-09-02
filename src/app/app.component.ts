0f65af549806239e6ea7a0406d904883f122f618
unknown <lucasvieirasouzaa@gmail.com>
2024-09-02 16:12:49 -0300

    Finishied portifolio is almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,27 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatTooltipModule,MatProgressBarModule],
+  imports: [RouterOutlet, MatTooltipModule, MatProgressBarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = 'portifolio-lucas';
+
+  WhatsApp() {
+    var x = "5531975580097";
+    var mensagem = "Hi Lucas Souza, I saw your portfolio and I saw that you are into FullStack Development technologies,lets get in touch!";
+    var link = "https://api.whatsapp.com/send?phone=" + x + "&text=" + encodeURIComponent(mensagem);
+    window.open(link);
+  }
+
+  Email() {
+    const email = "lucasvieirasouzaa@gmail.com";
+    const subject = "Hi Lucas Souza, I saw your portfolio and would like to get in touch!";
+    const subjectEncoded = encodeURIComponent(subject);  
+    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subjectEncoded}`;
+    window.location.href = gmailUrl;
+};
+
 }
